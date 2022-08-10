b91ae72332ef94ee6f5fcee566ca95b96ef1f27e
OMORE ABDULSALAM <101834898+Omore-Salam@users.noreply.github.com>
2022-08-10 02:45:21 +0100

    Create Spinner.jsx

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,23 @@
+ 
+
+ import spinner from '../assets/images/Ball-Spinner.svg' 
+
+  
+
+ function Spinner() { 
+
+   return ( 
+
+     <div className="loadingSpinnerContainer"> 
+
+       <img src={spinner} alt="loading spinner" /> 
+
+     </div> 
+
+   ) 
+
+ } 
+
+  
+
+ export default Spinner
